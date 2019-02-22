2800170364acb758cea5ab846aaf7b0b20244c11
harshkeshwala <keshwalaharsh@gmail.com>
2019-02-21 22:17:08 -0500

    Spin Random Images Working

--- SpriteKit-SlotMachine/GameScene.swift
+++ SpriteKit-SlotMachine/GameScene.swift
@@ -17,6 +17,12 @@ class GameScene: SKScene {
      var quitbutton: QuitButton?
      var resetbutton: ResetButton?
     
+     var firstImage = SKSpriteNode()
+     var secondImage = SKSpriteNode()
+     var thirdImage = SKSpriteNode()
+    
+     var textures = [SKTexture]()
+    
     override func didMove(to view: SKView) {
         
         // adding background
@@ -46,6 +52,29 @@ class GameScene: SKScene {
         resetbutton?.position = CGPoint(x: 610, y: -1160)
         resetbutton?.zPosition = 1
         addChild(resetbutton!)
+        
+        
+//        firstImage.position = CGPoint(x: 220, y: -600)
+//        secondImage.position = CGPoint(x: 370, y: -600)
+//        thirdImage.position = CGPoint(x: 620, y: -600)
+        
+        firstImage = (self.childNode(withName: "image1") as? SKSpriteNode)!
+        secondImage = (self.childNode(withName: "image2") as? SKSpriteNode)!
+        thirdImage = (self.childNode(withName: "image3") as? SKSpriteNode)!
+        
+//        addChild(firstImage)
+//        addChild(secondImage)
+//        addChild(thirdImage)
+        
+        textures.append(SKTexture(imageNamed: "Kiwi"))
+        textures.append(SKTexture(imageNamed: "Lemon"))
+        textures.append(SKTexture(imageNamed: "Mango"))
+        textures.append(SKTexture(imageNamed: "Grape"))
+//        let image = UIImage(named: "Grape")
+//        let texture = SKTexture(image: image!)
+//        firstImage = SKSpriteNode(texture: texture)
+//        firstImage.position = CGPoint(x: 220, y: -600)
+//        addChild(firstImage)
     }
     
   
@@ -55,7 +84,18 @@ class GameScene: SKScene {
             let location = touch.location(in: self)
             let touchedNode = atPoint(location)
             if touchedNode.name == "spin" {
-                print("spin works!")
+                
+                let rand = Int(arc4random_uniform(UInt32(textures.count)))
+                let rand2 = Int(arc4random_uniform(UInt32(textures.count)))
+                let rand3 = Int(arc4random_uniform(UInt32(textures.count)))
+               
+                let texture = textures[rand] as SKTexture
+                let texture2 = textures[rand2] as SKTexture
+                let texture3 = textures[rand2] as SKTexture
+                
+                firstImage.texture = texture
+                secondImage.texture = texture2
+                thirdImage.texture = texture3
             }
             else if touchedNode.name == "quit"{
                 quitGame()
@@ -74,3 +114,4 @@ class GameScene: SKScene {
         // Called before each frame is rendered
     }
 }
+
--- SpriteKit-SlotMachine/GameViewController.swift
+++ SpriteKit-SlotMachine/GameViewController.swift
@@ -11,32 +11,9 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
-    
-    var images = [slotComp]()
-    var counter = 0
-    var comp1 = ""
-    var comp2 = ""
-    var comp3 = ""
-    var timer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        let img1  = slotComp(image: UIImage(named: "Kiwi"), color: "green")
-        let img2 = slotComp(image: UIImage(named: "Mango"), color: "red")
-        let img3 = slotComp(image: UIImage(named: "Grape"), color: "blue")
-        
-        images = [img1, img2, img3,img2, img2, img3,img1, img2, img3,img1,
-                  img2, img1,img1, img2, img3,img3, img2, img3,img1, img1, img3,
-                  img3, img2,img3, img1, img3,img3, img1, img2,img1, img1, img2,
-                  img3, img2,img1, img3, img3,img2, img3, img1,img2, img3, img1,
-                  img2, img1,img1, img2, img3,img1, img3, img3,img1, img2, img3,
-                  img1,img1, img3, img1, img1, img3]
-        
-
-        srandom(UInt32(time(nil)))
         
         
         if let view = self.view as! SKView? {
@@ -55,12 +32,7 @@ class GameViewController: UIViewController {
             view.showsNodeCount = true
         }
     }
-    struct slotComp{
-        var image: UIImage!
-        var color: String
-    }
-    
-    
+   
     override var shouldAutorotate: Bool {
         return true
     }
